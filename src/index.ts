c0141b9a463f514f9a09562c65a26384db9986ae
PannH <pannh2609@gmail.com>
2023-08-14 16:37:01 +0200

    export the getUsageExamples function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import getDefinitionGroups from './functions/getDefinitionGroups';
 import getPronunciation from './functions/getPronunciation';
 import getConjugationGroups from './functions/getConjugationGroups';
+import getUsageExamples from './functions/getUsageExamples';
 
 export {
    getDefinitionGroups,
    getPronunciation,
-   getConjugationGroups
+   getConjugationGroups,
+   getUsageExamples
 }
