f317bee9ee040307fcb6ddd78cc3f81adb95afe7
PannH <pannh2609@gmail.com>
2023-08-14 15:37:21 +0200

    create the getPronunciation function

---
+++ src/functions/getPronunciation.ts
@@ -0,0 +1,32 @@
+import createPage from '../helpers/createPage';
+import getElementProperty from '../helpers/getElementProperty';
+import type Pronunciation from '../interfaces/Pronunciation';
+
+export default async function getPronunciation(wordQuery: string): Promise<Pronunciation> {
+
+   const pageURL = `https://dictionnaire.lerobert.com/definition/${encodeURIComponent(wordQuery)}`;
+
+   const { browser, page } = await createPage(pageURL);
+
+   const definitionsContainer = await page.$('#definitions');
+
+   if (!definitionsContainer) {
+
+      await browser.close();
+
+      throw new Error(`No definitions found for "${wordQuery}"`);
+
+   }
+
+   const audioSourceURL = await getElementProperty<HTMLSourceElement, string>(
+      await page.$('audio > source'),
+      'src'
+   );
+
+   await browser.close();
+
+   return {
+      audioURL: audioSourceURL
+   }
+
+}
