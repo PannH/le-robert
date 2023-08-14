75dbc84c7fc3f0fa8d851eb9c204f43b3e7874d9
PannH <pannh2609@gmail.com>
2023-08-14 18:07:05 +0200

    export the types

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,29 @@ import getDefinitionGroups from './functions/getDefinitionGroups';
 import getPronunciation from './functions/getPronunciation';
 import getConjugationGroups from './functions/getConjugationGroups';
 import getUsageExamples from './functions/getUsageExamples';
+import type DefinitionGroup from './interfaces/DefinitionGroup';
+import type Definition from './interfaces/Definition';
+import type Pronunciation from './interfaces/Pronunciation';
+import type ConjugationGroup from './interfaces/ConjugationGroup';
+import type ConjugationSubgroup from './interfaces/ConjugationSubgroup';
+import type ConjugationTense from './interfaces/ConjugationTense';
+import type UsageExample from './interfaces/UsageExample';
+import type UsageExampleSource from './interfaces/UsageExampleSource';
 
 export {
+   // Functions
    getDefinitionGroups,
    getPronunciation,
    getConjugationGroups,
-   getUsageExamples
+   getUsageExamples,
+
+   // Types
+   DefinitionGroup,
+   Definition,
+   Pronunciation,
+   ConjugationGroup,
+   ConjugationSubgroup,
+   ConjugationTense,
+   UsageExample,
+   UsageExampleSource
 }
