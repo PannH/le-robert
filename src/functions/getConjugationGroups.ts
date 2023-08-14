fee36730f4123a2b5fe1d1e09759ecdf1df11ea0
PannH <pannh2609@gmail.com>
2023-08-14 16:11:03 +0200

    create the getConjugationGroups function

---
+++ src/functions/getConjugationGroups.ts
@@ -0,0 +1,86 @@
+import createPage from '../helpers/createPage';
+import getElementProperty from '../helpers/getElementProperty';
+import type ConjugationGroup from '../interfaces/ConjugationGroup';
+import ConjugationTense from '../interfaces/ConjugationTense';
+
+export default async function getConjugationGroups(verbQuery: string): Promise<ConjugationGroup[]> {
+
+   const pageURL = `https://dictionnaire.lerobert.com/conjugaison/${encodeURIComponent(verbQuery)}`;
+
+   const { browser, page } = await createPage(pageURL);
+
+   const conjugationsContainer = await page.$('.conj_verbe');
+
+   if (!conjugationsContainer) {
+
+      await browser.close();
+
+      throw new Error(`No conjugations found for "${verbQuery}"`);
+
+   }
+
+   const conjugationGroups: ConjugationGroup[] = [];
+
+   for (const group of await page.$$('.conj')) {
+
+      const groupName = await getElementProperty<HTMLHeadingElement, string>(
+         await group.$('h2'),
+         'textContent'
+      );
+
+      const conjugationGroup: ConjugationGroup = {
+         name: groupName,
+         subgroups: []
+      }
+
+      for (const subgroupNameElement of await group.$$('.cf-b')) {
+
+         const subgroupName = await getElementProperty<Element, string>(subgroupNameElement, 'textContent');
+
+         const tenses = await subgroupNameElement.evaluate((element) => {
+
+            const sibling = element.nextElementSibling;
+            const tenses: ConjugationTense[] = [];
+
+            for (let i = 0; i < sibling.childElementCount; i++) {
+
+               const children = sibling.children[i];
+
+               const tenseName = children.querySelector('h4').textContent;
+               const conjugationsCollection = children.querySelectorAll('p');
+
+               const conjugations = [];
+
+               for (let i = 0; i < conjugationsCollection.length; i++) {
+
+                  const conjugationItem = conjugationsCollection[i];
+
+                  conjugations.push(conjugationItem.textContent)
+
+               }
+
+               tenses.push({
+                  name: tenseName,
+                  conjugations
+               });
+
+            }
+
+            return tenses;
+
+         });
+
+         conjugationGroup.subgroups.push({
+            name: subgroupName,
+            tenses
+         });
+
+      }
+
+      conjugationGroups.push(conjugationGroup);
+
+   }
+
+   return conjugationGroups;
+
+}
